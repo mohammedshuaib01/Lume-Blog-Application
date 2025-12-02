f412ce6275e4faf807c6b5161836e142308a98ff
Shuaib <mohammedshuaibka@gmail.com>
2025-12-02 15:35:00 +0530

    Allow all hosts in settings.py

--- lume_backend/lumebackend/lumebackend/settings.py
+++ lume_backend/lumebackend/lumebackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a693rcciz=t3kc$comu2@mo=r@^78lx05_6behx5b+#nxb)%j@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
