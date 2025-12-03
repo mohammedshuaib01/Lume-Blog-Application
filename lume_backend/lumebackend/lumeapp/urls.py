0902df9121e6de188c12e0e4c76f0a71b948038a
shuaib <mohammedshuaibka@gmail.com>
2025-12-04 00:16:27 +0530

    latest render with url base

--- lume_backend/lumebackend/lumeapp/urls.py
+++ lume_backend/lumebackend/lumeapp/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from .views import PostViewSet, UploadImageView, UploadVideoView
 
 router = routers.DefaultRouter()
-router.register(r'posts', PostViewSet)
+router.register(r'posts', PostViewSet, basename='post')
 
 urlpatterns = [
     path('', include(router.urls)),
